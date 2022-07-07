39ea7576be841a26609e017e32678475b4f99a6a
!!false <jinchengz202011@163.com>
2022-07-07 09:17:19 +0800

    feat: alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from './components/button'
+import { Alert } from './components/alert'
 
 function App() {
 
@@ -8,6 +9,8 @@ function App() {
       <Button size='sm' btnType='primary'>Primary button</Button>
       <Button btnType='danger' disable>Danger button</Button>
       <Button btnType='link' href='1'>Link button</Button>
+
+      <Alert title='2323' content='123434' alertType='danger'/>
     </div>
   );
 }
--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import classNames from 'classnames';
 
-type BaseAlertProps = 'danger' | 'warning' | 'success' | 'default';
+type BaseAlertProps = 'danger' | 'warning' | 'success' | 'info';
 
-export const Alert = () => {
-  return <></>
+interface AlertProps {
+  className?: string;
+  alertType?: BaseAlertProps;
+  close?: boolean;
+  title?: string | undefined;
+  content: string;
+  icon?: React.ReactNode | undefined;
+}
+
+export const Alert: React.FC<AlertProps> = (props) => {
+  const {
+    className,
+    alertType,
+    close,
+    title,
+    content,
+    icon
+  } = props;
+
+  const alertRef = useRef<any>();
+
+  const classes = classNames('alert', className, {
+    [`alert-${alertType}`]: alertType,
+  })
+
+  const closeAlert = () => {
+    alertRef?.current?.remove();
+  }
+
+  return <div className={classes} ref={alertRef}>
+    <div className='header'>
+      {/* icon图标&标题 */}
+      <div className='icon-title'>
+        {icon && <span className='icon'>{icon}</span>}
+        {title && <span className='title'>{title}</span>}
+      </div>
+      {/* 关闭按钮 */}
+      {
+        close && <div className='closeIcon'>
+          <span style={{ display: 'block' }} onClick={closeAlert}>X</span>
+        </div>
+      }
+    </div>
+    {/* 内容 */}
+    <div className='content'>
+      {content}
+    </div>
+  </div>
+}
+
+Alert.defaultProps = {
+  alertType: 'info',
+  close: true
 }
