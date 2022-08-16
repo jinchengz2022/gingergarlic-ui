c9192489c855fc564a9b6978df012c1b95e8d2fe
!!false <jinchengz202011@163.com>
2022-08-16 19:20:01 +0800

    fix: directory structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style/index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.scss'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export * from './components/alert'
+export * from './components/menu'
+export * from './components/tabs'
+export * from './components/button'
+export * from './components/input'
+export * from './components/select'
+export * from './components/upload'
+export * from './components/modal'
+export { default as Form } from './components/form'
