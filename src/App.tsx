ae95472566500450e3e638c157c0e52c1ca52321
!!false <jinchengz202011@163.com>
2022-08-16 19:24:08 +0800

    fix: directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
 import React from 'react';
-import { Form, Input, Select } from '../src/components'
 
 function App() {
 
   return (
     <div>
-      <Form>
-        <Form.Item
-          label='name'
-          name='name'
-          rules={[
-            {
-              required: true,
-              message: 'hhh',
-              validator: (rules, value) => new Promise((res, rej) => {
-                res(value);
-              })
-            }
-          ]}
-        >
-          {(values) => <Input />}
-        </Form.Item>
-        <Form.Item label='age'>
-          <Select options={[{ value: '10', label: '10' }]} />
-        </Form.Item>
-        <Form.Item name='checked'>
-          <button type='submit'>submit</button>
-        </Form.Item>
-      </Form>
     </div>
   );
 }
