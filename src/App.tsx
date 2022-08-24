7cfa4123ac11142aecc4a0ca5ac32ce57e7b8955
!!false <jinchengz202011@163.com>
2022-08-24 22:06:02 +0800

    style: 步骤条样式修改

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ function App() {
 
   return (
     <div>
-      <Steps current={1}>
+      <Steps current={3}>
         <Steps.Step title='first step' description='139139003332200313900333223322'/>
         <Steps.Step title='second step' description='jfdsjfoidsjfiosdofh'/>
-        <Steps.Step title='three step' description='哈哈哈'/>
+        <Steps.Step title='three step' description='哈哈哈' state='error'/>
+        <Steps.Step title='four step' description='嘻嘻嘻'/>
       </Steps>
     </div>
   );
--- src/components/steps/Step.tsx
+++ src/components/steps/Step.tsx
@@ -18,20 +18,26 @@ export const Step: FC<StepProps> = props => {
   const [stepCurrent, updateCurrent] = React.useState(current ?? 1);
   const [stepState, updateState] = React.useState(state);
 
-  const classes = classNames('step', {})
-
-  const stepStateClasses = classNames('step-state', `step-${stepState}`)
+  const stepStateClasses = classNames('step-state', {
+    'step-finish': index! + 1 < stepCurrent,
+    'step-process': (index! + 1 === stepCurrent) ||
+      (stepCurrent === 1) ||
+      (stepCurrent === childrenLen),
+    'step-error': state === 'error',
+    'step-wait': index! + 1 > stepCurrent,
+  })
 
   const stepDescClasses = classNames('step-desc', {})
 
   const stepTitleClasses = classNames('step-title', {
     'line': index! < childrenLen! - 1,
     [`step-title-${stepState}`]: true,
-    [`line-${stepState}`]: true
+    'step-line-wait': index! + 1 > stepCurrent,
+    'step-line-error': stepState === 'error',
   })
 
   return (
-    <div className={classes}>
+    <div className='step'>
       <div className={stepStateClasses}>{index! + 1}</div>
       <div className='step-wrapper'>
         <div className={stepTitleClasses}>{title}</div>
